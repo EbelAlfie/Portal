19a2207d11daf0a0fba38836c10c92aaf1cb6be5
EbelAlfie <davis.onde.wei@gmail.com>
2023-11-06 06:15:17 +0700

    test toast

--- app/src/main/java/com/share/portal/utils/ToastBuilder.kt
+++ app/src/main/java/com/share/portal/utils/ToastBuilder.kt
@@ -1,6 +1,45 @@
 package com.share.portal.utils
 
-class ToastBuilder {
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent.getService
+import android.content.Context
+import android.os.Binder
+import android.os.Handler
+import android.os.RemoteException
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.resources.Compatibility
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
+
+class ToastBuilder(private val context: Context) {
+
+  private val mToken: Binder? = null
+  private val mHandler: Handler? = null
+  private var toastView: TextView? = null
+
+  fun show() {
+    if (toastView == null) return
+    val service = Notification.Builder(context)
+      .setAutoCancel(true)
+  }
+
+  fun setView(textView: TextView) {
+    toastView = textView
+  }
 
 
+  private fun getService() {
+  }
+  interface ToastCallback {
+    fun onShow()
+    fun onHide()
+  }
+
+  companion object {
+    const val TAG: String = "Toast"
+
+  }
 }
